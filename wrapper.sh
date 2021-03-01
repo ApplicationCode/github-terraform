b79c1a3b87a1f0b07602be3007a2ea590f1acfcd
Mayank Aggarwal <aggarwal.mayank@live.com>
2021-03-01 16:57:11 +0100

    wrapper file

--- wrapper.sh
+++ wrapper.sh
@@ -2,3 +2,16 @@
 
 
 echo "PROJECT_ID = $1"
+echo "BUILD_ID = $2"
+echo "COMMIT_SHA = $3"
+echo "REVISION_ID = $4"
+echo "SHORT_SHA = $5"
+echo "REPO_NAME = $6"
+echo "BRANCH_NAME = $7"
+echo "TAG_NAME = $8"
+echo "_HEAD_BRANCH = $9"
+echo "_BASE_BRANCH = ${10}"
+echo "_HEAD_REPO_URL = ${11}"
+echo "_PR_NUMBER = ${12}"
+#echo " = $"
+
